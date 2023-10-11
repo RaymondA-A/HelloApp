d5f7a2638d69170bf1a529ca4435b1617a0d5571
Raymond Adu <raymond.aduadjei@kibo.school>
2023-10-11 12:01:01 +0000

    second one

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!if I fully understand this you are finished'
 
+if __name__ == '__main__':
+    app.run(debug = True)
